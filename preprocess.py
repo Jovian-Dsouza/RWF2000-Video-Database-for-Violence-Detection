ca79f8cbc8d3d02399755b4821f8745e51875425
Jovian Dsouza <dsouzajovian123@gmail.com>
2021-06-20 15:21:55 +0530

    Preprocess and save

--- preprocess.py
+++ preprocess.py
@@ -73,7 +73,7 @@ def Video2Npy(file_path, resize=(224,224)):
     
     return result
 
-def Save2Npy(file_dir, save_dir):
+def Save2Npy(file_dir, save_dir, preprocess=True):
     """Transfer all the videos and save them into specified directory
     Args:
         file_dir: source folder of target videos
@@ -92,9 +92,43 @@ def Save2Npy(file_dir, save_dir):
         save_path = os.path.join(save_dir, video_name+'.npy') 
         # Load and preprocess video
         data = Video2Npy(file_path=video_path, resize=(224,224))
-        data = np.uint8(data)
+
+        if preprocess:
+            data = np.float32(data)
+            data = uniform_sampling(video=data, target_frames=64)
+            data[..., :3] = normalize(data[..., :3])
+            data[..., 3:] = normalize(data[..., 3:])
+        else:
+            data = np.uint8(data)
         # Save as .npy file
         np.save(save_path, data)
     
     return None
+
+def normalize(data):
+    mean = np.mean(data)
+    std = np.std(data)
+    return (data-mean) / std
+
+def uniform_sampling(video, target_frames=64):
+    # get total frames of input video and calculate sampling interval 
+    len_frames = int(len(video))
+    interval = int(np.ceil(len_frames/target_frames))
+    # init empty list for sampled video and 
+    sampled_video = []
+    for i in range(0,len_frames,interval):
+        sampled_video.append(video[i])     
+    # calculate numer of padded frames and fix it 
+    num_pad = target_frames - len(sampled_video)
+    padding = []
+    if num_pad>0:
+        for i in range(-num_pad,0):
+            try: 
+                padding.append(video[i])
+            except:
+                padding.append(video[0])
+        sampled_video += padding     
+    # get sampled video
+    return np.array(sampled_video, dtype=np.float32)
+
 
